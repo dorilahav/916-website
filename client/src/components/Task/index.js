a49e9887141e390e97c875880f20b4dd7eae24ce
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-08-16 07:26:46 +0300

    feat(tasks): add enter animation to tasks

--- client/src/components/Task/index.js
+++ client/src/components/Task/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import {Grid, Card, CardContent, Typography} from '@material-ui/core';
+import {Grid, Card, CardContent, Typography, makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  card: {
+    animation: '$enter 500ms'
+  },
+  '@keyframes enter': {
+    '0%': {
+      opacity: 0,
+      transform: 'scale(0.2)'
+    },
+    '100%': {
+      opacity: 1,
+      transform: 'scale(1)'
+    }
+  }
+});
 
 export default ({task}) => {
+  const classes = useStyles();
+
   return (
     <Grid item xs={12} sm={6} md={6} lg={4} xl={2}>
-      <Card>
+      <Card className={classes.card}>
         <CardContent>
           <Typography variant="h4">{task.title}</Typography>
           <Typography variant="body1">{task.description}</Typography>
