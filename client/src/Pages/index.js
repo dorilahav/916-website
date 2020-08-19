7712897279340ab985ac617dbaf334ec7da826c5
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-08-19 06:24:58 +0300

    feat(admin): create admin page

--- client/src/Pages/index.js
+++ client/src/Pages/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Home from './Home';
 import Tasks from './Tasks';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
+import Admin from './Admin';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 export default () => (
   <BrowserRouter>
     <Route exact path="/" component={Home}/>
     <Route exact path="/tasks" component={Tasks}/>
+    <Route exact path="/admin" component={Admin}/>
   </BrowserRouter>
 );
