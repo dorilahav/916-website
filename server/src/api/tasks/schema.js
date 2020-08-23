417db4f545d7214aa891d41c182a2c69185ab3ca
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-08-23 08:00:08 +0300

    feat(tasks/schema): add all fields to tasks schema

--- server/src/api/tasks/schema.js
+++ server/src/api/tasks/schema.js
@@ -1,7 +1,7 @@
 const yup = require('yup');
 import {TaskStatus, TaskCategory} from './enums';
 
-const pointsSchema = yup.object().noUnknown(true, 'An invalid category for points was entered').shape({
+const pointsSchema = yup.object().noUnknown(true, 'An invalid category was entered').shape({
   [TaskCategory.FIGHTING]: yup.number().notRequired(),
   [TaskCategory.STRATEGY]: yup.number().notRequired(),
   [TaskCategory.CREATIVE_THINKING]: yup.number().notRequired(),
@@ -9,10 +9,13 @@ const pointsSchema = yup.object().noUnknown(true, 'An invalid category for point
 });
 
 const schema = yup.object().noUnknown().shape({
-  title: yup.string().required('A task must include a title').max(100, 'Title must be less than ${max} characters long'),
-  description: yup.string().required('A task must include a description'),
-  status: yup.string().oneOf([...Object.values(TaskStatus)], 'An invalid status was entered').required('A task must include a status'),
-  points: pointsSchema.notRequired()
+  id: yup.string().required('Every task must have an id!').typeError('Invalid id was provided'),
+  title: yup.string().required('A task must have a title').max(100, 'Title must be less than ${max} characters long').typeError('Invalid title was provided'),
+  description: yup.string().required('A task must have a description').typeError('Invalid description was provided'),
+  points: pointsSchema.default({}).typeError('Invalid points object was provided'),
+  status: yup.string().oneOf([...Object.values(TaskStatus)], 'Invalid status was provided').default(TaskStatus.PENDING).required('A task must have a status').typeError('Invalid status was provided'),
+  requiredTasks: yup.array().of(yup.string()).notRequired().typeError('Invalid requiredTasks array was provided'),
+  unlocked: yup.boolean().default(false).typeError('Invalid unlocked state was provided')
 });
 
 export default schema;
