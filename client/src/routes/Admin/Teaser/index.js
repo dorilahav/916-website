1cc3ad7bedae8fa06c0123a9b1f6f7dc22a0240d
Dori Lahav Waisberg <dori.lahav@gmail.com>
2020-08-27 06:56:35 +0300

    feat(Teaser): added teaser to website

---
+++ client/src/routes/Admin/Teaser/index.js
@@ -0,0 +1,65 @@
+import 'moment-duration-format';
+import moment from 'moment';
+import React, {useState, useEffect} from 'react';
+import {Box, Typography, makeStyles} from '@material-ui/core';
+import cyber from '../../assets/cyber.jpg';
+
+const useStyles = makeStyles({
+  background: {
+    position: 'relative',
+    '&:after': {
+      content: '""',
+      backgroundImage: `url(${cyber})`,
+      opacity: 0.15,
+      zIndex: -1,
+      position: 'absolute',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0
+    }
+  },
+  title: {
+    fontWeight: 'bold'
+  },
+  space: {
+    margin: '50px 0',
+    color: 'lightblue'
+  }
+});
+
+export default () => {
+  const classes = useStyles();
+
+  const [endTime, setEndTime] = useState(moment("8/29/2020 12:30", "M/D/YYYY H:mm"));
+  const [countdown, setCountdown] = useState('');
+
+  const updateTime = () => {
+    const duration = moment.duration(endTime.diff(moment.now()), 'milliseconds');
+    const formattedDuration = duration.format('DD:hh:mm:ss', 0, {trim: false});
+
+    setCountdown(formattedDuration);
+  };
+
+  useEffect(() => {
+    updateTime();
+
+    setInterval(updateTime, 500)
+  }, []);
+
+  return (
+    <Box height="100%" display="flex" alignItems="center" justifyContent="center">
+      <Box dir="rtl" display="flex" flexDirection="column" alignItems="center" justifyContent="space-around" height="100%" className={classes.background}>
+        <Typography variant="h2">
+            המטרה שלכם היא להציל את המדינה, תעשו כל שביכולתכם להצליח במשימה החשובה הזו!
+          </Typography>
+          <Typography variant="h1" className={classes.title}>
+            אתם מוכנים??
+          </Typography>
+          <Typography align="left" variant="h3" className={classes.space}>
+            {countdown}
+          </Typography>
+      </Box>
+    </Box>
+  );
+}
